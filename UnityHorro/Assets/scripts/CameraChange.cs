b2193055c543eaef63a07e638a69f57952a6f5e5
CODman-2019 <55792105+CODman-2019@users.noreply.github.com>
2020-02-10 15:43:22 -0400

    Horror game project in the works

---
+++ UnityHorro/Assets/scripts/CameraChange.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraChange : MonoBehaviour
+{
+    public GameObject Original;
+    public GameObject Newcam;
+
+    private void Update()
+    {
+     
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("CameraSwap complete");
+            Original.SetActive(false);
+            Newcam.SetActive(true);
+
+            GameObject temp = Original;
+            Original = Newcam;
+            Newcam = temp;
+        }
+    }
+}
---
+++ UnityHorro/Assets/scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float walk;
+    public float run;
+    public float turn;
+
+    float speed;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        speed = walk;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            IsRunning();
+            gameObject.transform.Translate(Vector3.forward * speed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            gameObject.transform.Translate(Vector3.back * speed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(new Vector3 (0.0f, -turn, 0.0f));
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(new Vector3(0.0f, turn, 0.0f));
+        }
+    }
+
+    void IsRunning()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = run;
+        }
+    }
+}
