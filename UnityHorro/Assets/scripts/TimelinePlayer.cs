213cae5a2b030ed22ee0146cf5229be48e6a50a5
CODman-2019 <55792105+CODman-2019@users.noreply.github.com>
2020-02-20 15:51:38 -0400

    lights added

---
+++ UnityHorro/Assets/scripts/TimelinePlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class TimelinePlayer : MonoBehaviour
+{
+    public PlayableDirector timeline;
+
+    public bool OneTimeTrigger = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        timeline.Play();
+        Debug.Log("YEAH!");
+
+        if (OneTimeTrigger == true)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
