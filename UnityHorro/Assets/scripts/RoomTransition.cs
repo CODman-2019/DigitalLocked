c9bf820c9f239e5f86050e643f38285bd6c581bd
CODman-2019 <55792105+CODman-2019@users.noreply.github.com>
2020-03-03 11:48:52 -0400

    starting 2nd floor

---
+++ UnityHorro/Assets/scripts/RoomTransition.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomTransition : MonoBehaviour
+{
+    public GameObject newLocation;
+
+    GameObject current;
+
+    private void Start()
+    {
+        current = newLocation;
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.transform.position = current.transform.position;
+        }
+    }
+}
